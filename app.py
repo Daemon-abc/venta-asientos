ec632b3ac1139f7dcb69a90892cf235d0a26c63a
Daemon-abc <roney.cmr@gmail.com>
2026-06-03 00:50:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ from datetime import datetime, timedelta
 
 def generar_imagen_comprobante(b):
     try:
-        img = Image.open("Boleto9.png").convert('RGB') 
+        img = Image.open("ticked-09.png").convert('RGB') 
     except FileNotFoundError:
-        img = Image.new('RGB', (400, 500), color=(255, 255, 255))
+        img = Image.new('RGB', (800, 800), color=(255, 255, 255))
     
     d = ImageDraw.Draw(img)
     
     try:
-        fuente_titulo = ImageFont.truetype("Ticketing.ttf", 20)
-        fuente_texto = ImageFont.truetype("Ticketing.ttf", 35)
+        fuente_titulo = ImageFont.truetype("Ticketing.ttf", 60)
+        fuente_texto = ImageFont.truetype("Ticketing.ttf", 20)
     except IOError:
         fuente_titulo = ImageFont.load_default()
         fuente_texto = ImageFont.load_default()
@@ -37,14 +37,14 @@ def generar_imagen_comprobante(b):
     else:
         fecha_mostrar = "No registrada"
 
-    # --- DIBUJAR LOS TEXTOS ---
-    d.text((130, 625), f"ASIENTO: {b.get('Asiento')}", fill=(255, 255, 255), font=fuente_texto)
-    d.text((130, 670), f"FILA: {b.get('Fila')}", fill=(255, 255, 255), font=fuente_texto)
-    d.text((130, 715), f"ZONA: {b.get('Zona')}", fill=(255, 255, 255), font=fuente_texto)
-    d.text((130, 760), f"CLIENTE: {b.get('Datos Cliente')}", fill=(255, 255, 255), font=fuente_texto)
-    d.text((130, 805), f"VENDEDOR: {b.get('Vendedor')}", fill=(255, 255, 255), font=fuente_texto)
-    d.text((130, 850), f"FECHA VENTA: {fecha_mostrar}", fill=(255, 255, 255), font=fuente_texto)
-    
+  # --- DIBUJAR LOS TEXTOS CENTRADOS ---
+
+    d.text((400, 470), f"ASIENTO: {b.get('Asiento')}", fill=(255, 255, 255), font=fuente_titulo, anchor="mm")
+    d.text((400, 530), f"FILA: {b.get('Fila')}", fill=(255, 255, 255), font=fuente_titulo, anchor="mm")
+    d.text((400, 590), f"ZONA: {b.get('Zona')}", fill=(255, 255, 255), font=fuente_titulo, anchor="mm")
+    d.text((400, 650), f"CLIENTE: {b.get('Datos Cliente')}", fill=(255, 255, 255), font=fuente_texto, anchor="mm")
+    d.text((400, 675), f"VENDEDOR: {b.get('Vendedor')}", fill=(255, 255, 255), font=fuente_texto, anchor="mm")
+    d.text((400, 700), f"FECHA VENTA: {fecha_mostrar}", fill=(255, 255, 255), font=fuente_texto, anchor="mm")
     buf = io.BytesIO()
     img.save(buf, format='PNG')
     return buf.getvalue()
